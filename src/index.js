2954ce2a9a4ccec50b5382994c42a65c6294c9c2
Lucia Nina <lucianina1994@gmail.com>
2022-04-24 11:58:42 -0300

    UPDATE las card se puden eliminar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const button = document.querySelector("#submit");
+
 const input = document.querySelector("#country");
 const app = document.querySelector('#weather')
 const key = "dffac76febe572281ea864fb119d7d27";
@@ -11,7 +12,7 @@ const accion = () => {
     .then((response) => response.json())
     .then((data) => {
             const container = document.createElement('div')
-            container.className = "mt-2 p-2 w-40 mx-auto bg-black rounded-md shadow-lg shadow-blue-500/50"
+            container.className = "mt-2 p-2 w-40 mx-auto bg-black rounded-md shadow-lg shadow-blue-500/50 relative"
             const weather = data.weather[0]
             console.log(data.main.temp);
 
@@ -31,13 +32,26 @@ const accion = () => {
             elCelcius.textContent = "°C"
             elCelcius.className = "text-sm absolute top-1 tracking-normal"
             elTemperatura.append(elCelcius)
+
             let elIcon = document.createElement('img')
             elIcon.className = "mx-auto -mt-2"
             elIcon.src = ` http://openweathermap.org/img/wn/${weather.icon}@2x.png`
-            container.append(elNombre,elTemperatura,elIcon,elClimaDesc)
+
+            let elClose = document.createElement('span')
+            elClose.textContent = "x"
+            elClose.className = "absolute text-white top-0 font-semibold right-2 p-1"
+            elClose.id = "close"
+            elClose.addEventListener("click",remove)
+            
+            container.append(elClose,elNombre,elTemperatura,elIcon,elClimaDesc)
             app.append(container)
             
         });
 };
 
+const remove = (x) =>{
+    const parent = x.target.parentNode;
+    parent.remove()
+}
+const btn_close = document.querySelector("#close")
 button.addEventListener("click", accion);
